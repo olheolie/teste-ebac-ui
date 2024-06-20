d9c70da5854dd236e9b3c412a292b2934b2004ae
Oliver <biatm.teles@gmail.com>
2024-06-20 16:36:13 -0300

    testes automatizados cypress

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,21 @@
+/// <reference types= "cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    before(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista ', () => {
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Argus All-Weather Tank')
+        .click()
+        
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+
+    });
+
+});
