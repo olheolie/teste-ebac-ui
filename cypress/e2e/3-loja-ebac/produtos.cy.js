31a16ab4413781a9d16101e20237cacb625e35c8
Oliver <biatm.teles@gmail.com>
2024-06-27 16:04:56 -0300

    Massa de dados e Lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,14 +12,35 @@ describe('Funcionalidade: Produtos', () => {
 
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Aero Daily Fitness Tee'
         produtosPage.buscarProduto(produto)
-        cy.get('#tab-title-description > a').should('contain', produto)
-
+        cy.get('.product_title').should('contain', produto)
+    });
+    
+    it('|Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 4
+        produtosPage.visitarProduto('Aether Gym Pant')
+        produtosPage.addProdutoCarrinho('32', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Aether Gym Pant” foram adicionados no seu carrinho.')
+
+    });
+    it.only('Deve adicionar produto ao carrinho', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.visitarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho,
+                dados[0].cor,
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+    })
+
+       
+
     });
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -15,11 +15,17 @@ class ProdutosPage{
 
     }
 
-    visitarProduto(){
+    visitarProduto(nomedoProduto){
+        //cy.visit(`produtos/${nomedoProduto}`)
 
+        const urlFormatada = nomedoProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
-    addProdutoCarrinho() {
-
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 }
 export default new ProdutosPage
