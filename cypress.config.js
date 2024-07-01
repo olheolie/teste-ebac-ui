6c61665cfda24e5bdf6b651064ff673ab87b5935
Oliver <biatm.teles@gmail.com>
2024-07-01 16:22:16 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '97w3d2',
   e2e: {
-    baseUrl: "http://lojaebac.ebaconline.art.br/",
+ baseUrl: "http://lojaebac.ebaconline.art.br/",
+video: true,
+  setupNodeEvents(on, config) {
 
-    setupNodeEvents(on, config) {
   
-    
-  
-     
-  
-     
-  
-    },
-  
-   },
-  
-  });
+
+   
+
+   
+
+  },
+
+ },
+
+});
